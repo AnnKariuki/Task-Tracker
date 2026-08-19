f8a7f9a7150607b34484e1c96b7801c478e9a5da
Ann Kariuki <ann.kariuki@duke.edu>
2026-08-18 21:11:45 -0700

    use subparsers and subcommands to handle the different functions the program does

--- task.py
+++ task.py
@@ -31,13 +31,13 @@ def is_database_empty() -> bool:
     return False
 
 
-def add_task(description):
+def add_task(args):
     dt = datetime.datetime.now()
     dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
     task_id = str(uuid.uuid4())
     task = {
         "id":task_id,
-        "description": description,
+        "description": args.new_description,
         "createdAt": dt_str,
         "updatedAt": dt_str,
         "status": "todo"
@@ -58,12 +58,26 @@ def add_task(description):
         sys.exit(1)
     print(F"Task added successfully (ID:{task_id} )")
 
+def update_task(args):
+    print(args.task_id)
+    print(args.updated_description)
+
 def main() -> None:
     parser = argparse.ArgumentParser(prog="Taskly", description="Welcome to taskly. Your one stop shop to track and manage your tasks", epilog="Thanks for visiting")
-    parser.add_argument('action', choices=['add', 'update'], help="What action would you like to perform")
-    # choices didn't work because I need different arguments for each choice. how could they do update taskid when description is required
-    parser.add_argument("description", help="what's the task?")
+    subparsers = parser.add_subparsers(title="What you can do in taskly")
+
+    add_subparser = subparsers.add_parser('add', help='To add a task run: program_name add task_description')
+    add_subparser.add_argument('new_description')
+    add_subparser.set_defaults(func=add_task)
+
+    update_subparser = subparsers.add_parser('update', help='To update a task run: program_name task_id updated_description')
+    update_subparser.add_argument('task_id')
+    update_subparser.add_argument('updated_description')
+    update_subparser.set_defaults(func=update_task)
+
+    # parse the args and call whatever function was selected
     args = parser.parse_args()
-    add_task(args.description)
+    args.func(args)
+
 
 main()
