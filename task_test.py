4547e68ef761491c62ff5c89ba23cbc72e503b85
Ann Kariuki <ann.kariuki@duke.edu>
2026-08-23 20:40:43 -0700

    remove task

--- task_test.py
+++ task_test.py
@@ -61,6 +61,7 @@ class TestTaskTrackerMethods(unittest.TestCase):
             },
         ]
         task.update_task(args)
+        mock_save_database.assert_called_once()
         mock_save_database.assert_called_with([
             {
                 "id": "1",
@@ -70,6 +71,27 @@ class TestTaskTrackerMethods(unittest.TestCase):
                 "status": "todo"
             }
         ])
+
+    @patch("task.is_database_empty")
+    @patch('task.current_timestamp')
+    @patch('task.load_database')
+    @patch('task.save_database')
+    def test_delete_task(self, mock_save_database, mock_load_database, mock_current_time, mock_database_empty):
+        mock_database_empty.return_value = False
+        mock_current_time.return_value = "2026-08-23 20:08:35"
+        args = argparse.Namespace(updated_description='buy rolls royce',task_id="1")
+        mock_load_database.return_value = [
+            {
+                "id": "1",
+                "description": "buy a company",
+                "createdAt": "2026-08-22 14:46:17",
+                "updatedAt": "2026-08-22 14:46:17",
+                "status": "todo"
+            },
+        ]
+        task.delete_task(args)
+        mock_save_database.assert_called_once()
+        mock_save_database.assert_called_with([])
 
 if __name__ == '__main__':
     unittest.main()
