5d0203cf1fd64eff0714bc1add49ebe705119eb4
Ann Kariuki <ann.kariuki@duke.edu>
2026-08-23 20:49:50 -0700

    update status test

--- task_test.py
+++ task_test.py
@@ -79,7 +79,7 @@ class TestTaskTrackerMethods(unittest.TestCase):
     def test_delete_task(self, mock_save_database, mock_load_database, mock_current_time, mock_database_empty):
         mock_database_empty.return_value = False
         mock_current_time.return_value = "2026-08-23 20:08:35"
-        args = argparse.Namespace(updated_description='buy rolls royce',task_id="1")
+        args = argparse.Namespace(task_id="1")
         mock_load_database.return_value = [
             {
                 "id": "1",
@@ -92,6 +92,33 @@ class TestTaskTrackerMethods(unittest.TestCase):
         task.delete_task(args)
         mock_save_database.assert_called_once()
         mock_save_database.assert_called_with([])
+
+    @patch('task.current_timestamp')
+    @patch('task.load_database')
+    @patch('task.save_database')
+    def test_update_task_status(self, mock_save_database, mock_load_database, mock_current_time):
+
+        mock_current_time.return_value = "2026-08-23 20:08:35"
+        mock_load_database.return_value = [
+            {
+                "id": "1",
+                "description": "buy a company",
+                "createdAt": "2026-08-22 14:46:17",
+                "updatedAt": "2026-08-22 14:46:17",
+                "status": "todo"
+            },
+        ]
+        task.update_status("done", "1")
+        mock_save_database.assert_called_once()
+        mock_save_database.assert_called_with([
+            {
+                "id": "1",
+                "description": "buy a company",
+                "createdAt": "2026-08-22 14:46:17",
+                "updatedAt": "2026-08-23 20:08:35",
+                "status": "done"
+            },
+        ])
 
 if __name__ == '__main__':
     unittest.main()
